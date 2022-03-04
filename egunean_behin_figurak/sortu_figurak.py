67ee52edd65fab9a2b329cb1dd47616b44ad7c37
Julen Etxaniz <juletxara@gmail.com>
2022-03-04 17:36:59 +0100

    improve figures code to generate more questions

--- egunean_behin_figurak/sortu_figurak.py
+++ egunean_behin_figurak/sortu_figurak.py
@@ -1,54 +1,111 @@
-from PIL import Image, ImageDraw
+"""This module generate figure images"""
 import random
+import argparse
+from PIL import Image, ImageDraw
 
-koloreak = ["red", "green", "blue"]
-canvas = (600, 400)
-scale = 1
-thumb = canvas[0]/scale, canvas[1]/scale
+def draw_figure(draw, figure, color, x, y, r):
+    """Draw a figure with given color, position and radius.
 
-
-n_x=range(1,13)
-n_y=range(1,9)
-pausoa_x=thumb[0]/(len(n_x)+1)
-pausoa_y=thumb[1]/(len(n_y)+1)
-r=16
-
-def marraztu_borobila(draw, x,y,r):
-    kolorea = random.randint(0,2)
-    draw.ellipse((x-r, y-r, x+r, y+r), fill=koloreak[kolorea])
-    return kolorea
-
-def marraztu_karratua(draw, x,y,r):
-    kolorea = random.randint(0,2)
-    draw.rectangle([x-r, y-r, x+r, y+r], fill=koloreak[kolorea])
-    return kolorea
-
-def marraztu_triangelua(draw, x,y,r):
-    kolorea = random.randint(0,2)
-    draw.polygon([(x-r,y+r), (x+r, y+r), (x,y-r)], fill=koloreak[kolorea])
-    return kolorea
+    Args:
+        draw (ImageDraw): ImageDraw object.
+        figure (str): figure name.
+        color (str): color name.
+        x (int): x position.
+        y (int): y position.
+        r (int): radius.
+    """
+    if figure == 'triangle':
+        draw.polygon([(x-r, y+r), (x+r, y+r), (x, y-r)], fill=color)
+    if figure == 'square':
+        draw.rectangle([x-r, y-r, x+r, y+r], fill=color)
+    if figure == 'circle':
+        draw.ellipse((x-r, y-r, x+r, y+r), fill=color)
 
 
-marraztu_figurak = [marraztu_triangelua, marraztu_karratua, marraztu_borobila]
+def create_image(output_path, x_len, y_len):
+    """Create an image of figures with given parameters.
 
-def marraztu_galdera(i):
-    im = Image.new('RGBA', canvas, (255, 255, 255, 255))
-    draw = ImageDraw.Draw(im)
-    # 0hirukia, 1laukia, 2borobila / 0red, 1green, 2blue
-    iz={'00':0, '01':0, '02':0, '10':0, '11':0, '12':0, '20':0, '21':0, '22':0}
+    Args:
+        output_path (str): path for output files.
+        x_len (int): number of figures in axis X.
+        y_len (int): number of figures in axis Y.
+    """
+    figures = ['triangle', 'square', 'circle']
+    colors = ['red', 'green', 'blue']
 
-    for y1 in n_y:
-        for x1 in n_x:
-            x=x1*pausoa_x
-            y=y1*pausoa_y
-            figura = random.randint(0,2)
-            color = marraztu_figurak[figura](draw, x,y,r)
-            iz[str(figura)+str(color)]+=1
+    canvas = (600, 400)
+    r = 32
+    scale = 2
+    thumb = canvas[0]/scale, canvas[1]/scale
+    image = Image.new('RGBA', canvas, (255, 255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    # 0 triangle, 1 square, 2 circle
+    # 0 red, 1 green, 2 blue
+    c = {'00': 0, '01': 0, '02': 0, '10': 0,
+         '11': 0, '12': 0, '20': 0, '21': 0, '22': 0}
 
-    im.thumbnail(thumb)
-    im.save('./figurak/fig_%d_%d_%d_%d_%d_%d_%d_%d_%d.png'%(iz['00'], iz['01'], iz['02'], iz['10'], iz['11'], iz['12'], iz['20'], iz['21'], iz['22']))
+    x_step = thumb[0]/(x_len+1)
+    y_step = thumb[1]/(y_len+1)
+
+    for y1 in range(1, y_len + 1):
+        for x1 in range(1, x_len + 1):
+            x = x1 * x_step
+            y = y1 * y_step
+            figure = random.randint(0, 2)
+            color = random.randint(0, 2)
+            draw_figure(draw, figures[figure], colors[color], x, y, r)
+            c[str(figure)+str(color)] += 1
+
+    image.thumbnail(thumb)
+    filename = f"{output_path}/fig_{c['00']}_{c['01']}_{c['02']}_{c['10']}_{c['11']}_{c['12']}_{c['20']}_{c['21']}_{c['22']}.png"
+    image.save(filename)
+
+
+def parse_arguments():
+    """Parse command line arguments.
+
+    Returns:
+        args: parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--x_len",
+        type=int,
+        default=6,
+        help="Number of figures in axis X.",
+    )
+
+    parser.add_argument(
+        "--y_len",
+        type=int,
+        default=4,
+        help="Number of figures in axis Y.",
+    )
+
+    parser.add_argument(
+        "--n",
+        type=int,
+        default=5,
+        help="Number of images created.",
+    )
+
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default='images',
+        help="Path for output files.",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Main function."""
+    args = parse_arguments()
+    for _ in range(args.n):
+        create_image(args.output_path, args.x_len, args.y_len)
+
 
 if __name__ == '__main__':
-    zenbat_galdera=range(0,20)
-    for galdera in zenbat_galdera:
-        marraztu_galdera(galdera)
+    main()
--- egunean_behin_figurak/sortu_galderak.py
+++ egunean_behin_figurak/sortu_galderak.py
@@ -1,46 +1,117 @@
+"""This module generates questions for images."""
+import argparse
 import os
-import random
 import csv
-from random import choice
+import random
 
 
-path='./examples/'
-galdera_oinarria = 'Zenbat %s %s?'
+def write_questions(questions, filename):
+    """Write questions to a csv file.
 
-figurak = ['hiruki', 'lauki', 'borobil']
-koloreak = ['gorri', 'berde', 'urdin']
+    Args:
+        questions (list): list of questions.
+        filename (str): path to the output file.
+    """
+    with open(filename, 'w', encoding='UTF-8', newline="") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['type', 'question', 'correct',
+                             'wrong1', 'wrong2', 'image'])
+        for question in questions:
+            spamwriter.writerow(question)
 
-def galderak_idatzi():
-    galderak=sortu_galderak()
-    with open('galderak.csv', 'w') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['Galdera','Erantzun zuzena', 'Erantzun okerra 1','Erantzun okerra 2','Fitxategia'])
-        for galdera in galderak:
-            spamwriter.writerow(galdera)
+
+def wrong_answers(correct):
+    """Create wrong answers for a given correct answer.
+
+    Args:
+        correct (int): correct answer.
+
+    Returns:
+        list: list of wrong answers.
+    """
+    answers = [correct]
+    answers.append(random.choice([i for i in range(1, 10) if i not in answers]))
+    answers.append(random.choice([i for i in range(1, 10) if i not in answers]))
+    return answers[1:]
 
 
-def erantzun_okerrak(zuzena):
-    erantzunak=[int(zuzena)]
-    erantzunak.append(choice([i for i in range(1,10) if i not in erantzunak]))
-    erantzunak.append(choice([i for i in range(1,10) if i not in erantzunak]))
-    return erantzunak[1:] #Erantzun okerrak soilik bueltatu
+def create_questions(image_path):
+    """Create questions for each image in the images_path directory.
 
-def sortu_galderak():
-    irudiak = os.listdir(path)
-    galderak_list=[]
+    15 questions are created for each image. 3 questions about figure shape.
+    3 questions about figure color. 9 questions about figure shape and color.
 
-    for izena in irudiak:
-        figura = random.randint(0,2)
-        kolorea = random.randint(0,2)
-        kolorea_txt = koloreak[kolorea]
-        figura_txt = figurak[figura]
-        posizioa = (figura*3)+kolorea
-        kopuruak = izena.split('.')[0].split('_')[1:]
-        galdera = galdera_oinarria%(figura_txt,kolorea_txt)
-        zuzena = kopuruak[posizioa]
-        okerrak = erantzun_okerrak(zuzena)
-        galderak_list.append([galdera, zuzena, okerrak[0],okerrak[1],izena])
-    return galderak_list
+    Args:
+        image_path (str): path to the directory with images.
+
+    Returns:
+        list: list of questions.
+    """
+    figures = ['triangle', 'square', 'circle']
+    colors = ['red', 'green', 'blue']
+    category = "Figures"
+    images = os.listdir(image_path)
+    questions = []
+
+    for image in images:
+        counts = list(map(int, image.split('.')[0].split('_')[1:]))
+
+        for i, figure in enumerate(figures):
+            question = f"How many {figure}s?"
+            correct = sum(counts[i * 3:(i+1) * 3])
+            wrong = wrong_answers(correct)
+            questions.append([category, question, correct,
+                              wrong[0], wrong[1], image])
+
+        for i, color in enumerate(colors):
+            question = f"How many {color} figures?"
+            correct = counts[i] + counts[i + 3] + counts[i + 6]
+            wrong = wrong_answers(correct)
+            questions.append([category, question, correct,
+                              wrong[0], wrong[1], image])
+
+        for i, figure in enumerate(figures):
+            for j, color in enumerate(colors):
+                question = f"How many {color} {figure}s?"
+                correct = counts[i * 3 + j]
+                wrong = wrong_answers(correct)
+                questions.append(
+                    [category, question, correct, wrong[0], wrong[1], image])
+
+    return questions
+
+
+def parse_arguments():
+    """Parse command line arguments image_path and filename.
+
+    Returns:
+        args: parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--image_path",
+        type=str,
+        default='images',
+        help="Path for input images.",
+    )
+    parser.add_argument(
+        "--filename",
+        type=str,
+        default='questions.csv',
+        help="Path for output file.",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    """Main function."""
+    args = parse_arguments()
+    questions = create_questions(args.image_path)
+    write_questions(questions, args.filename)
+
 
 if __name__ == '__main__':
-    galderak_idatzi()
+    main()
